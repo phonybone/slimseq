40c772824856bf50249a2911bcaab31de11d46c6
Victor Cassen <phonybone@gmail.com>
2009-10-19 12:07:43 -0700

    local fix

--- db/migrate/20091008184421_fix_shit.rb
+++ db/migrate/20091008184421_fix_shit.rb
@@ -1,11 +1,11 @@
 class FixShit < ActiveRecord::Migration
   def self.up
-    remove_column :stp_pipelines, :stp_pipeline_id
+#    remove_column :stp_pipelines, :stp_pipeline_id
     remove_column :stp_pipelines, :experiment_id
   end
 
   def self.down
-    add_column :stp_pipelines, :stp_pipeline_id, :integer
+#    add_column :stp_pipelines, :stp_pipeline_id, :integer
     add_column :stp_pipelines, :experiment_id, :integer
   end
 end
