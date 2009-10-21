7f36f3ffae1c0a9e8f08b53dbdf1180fc2dfd9e4
Victor Cassen <phonybone@gmail.com>
2009-10-21 14:12:22 -0700

    getting ready to push

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -16,4 +16,6 @@ class ApplicationController < ActionController::Base
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   # protect_from_forgery # :secret => 'c1e0cc867735b750b889ccc506dd107f'
+
+  require 'array_helpers'
 end
